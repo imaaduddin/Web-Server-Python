dbefc4cd5fdbb3f5758fa3381f25998c951353ad
Imaad Uddin <imaaduddin@icloud.com>
2021-05-31 23:32:52 -0500

    favicon

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
 def hello_world():
+  print(url_for("static", filename="spade.ico"))
   return render_template("index.htm")
 
 @app.route("/about.htm")
